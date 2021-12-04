c61d3667bf0f3bfc45d4421486a65c9c0db3e5d9
Raul <raulrufino0@gmail.com>
2021-12-04 17:45:11 -0300

    Add option to print playfield and stack

--- run.py
+++ run.py
@@ -3,19 +3,26 @@
 from sys import argv
 from src.interpreter import BefungeInterpreter
 
+def print_playfield(playfield):
+    result = ''
+    for line in playfield:
+        result += ''.join(line) + '\n'
+    print("Playfield:")
+    print(result)
+
 if __name__ == '__main__':
     befunge_file = argv[1]
-    print_queue = '-v' in argv
+    verbose = '-v' in argv
     code = ''
     with open(befunge_file, 'r') as f:
         code = f.read()
     iterpreter = BefungeInterpreter()
     iterpreter.load(code)
-    queue, output, err = iterpreter.run()
-    print(output)
+    queue, err = iterpreter.run()
     if err:
         raise err()
-    if print_queue:
-        print(queue)
+    if verbose:
+        print(f"\nQueue:\n{queue}")
+        print_playfield(iterpreter.playfield)
     
 
